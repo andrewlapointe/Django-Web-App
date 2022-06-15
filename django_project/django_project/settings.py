39dd15c9ec47a373b9f5e315144078ee2e0c2594
Andrew LaPointe <andrewrlapointe@gmail.com>
2022-06-15 12:43:37 -0600

    added generic settings file

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3UdtT*HEJHncfM9Lp%X0p@KR8N7z@6@Do0apcC9&WFYZmt0zsQ'
+# create secret key here
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
