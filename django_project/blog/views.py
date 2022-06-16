37afa09585087e6f16d4a8a8cb35b0afec937fbd
Andrew LaPointe <andrewrlapointe@gmail.com>
2022-06-15 18:12:23 -0600

    added dummy html

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'AndrewL',
+        'title': 'Blog Post 1',
+        'content': 'Content',
+        'date_posted': 'June 15, 2022',
+    },
+    {
+        'author': 'Boogy Man',
+        'title': 'Blog Post 2',
+        'content': 'Content 2',
+        'date_posted': 'June 15, 2022',
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>About</h1>')
+    return render(request, 'blog/about.html')
