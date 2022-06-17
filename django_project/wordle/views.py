e76d8aa8860128be72bba51ae141e976647c70de
Andrew LaPointe <andrewrlapointe@gmail.com>
2022-06-17 15:36:37 -0600

    added template/master structure

--- django_project/wordle/views.py
+++ django_project/wordle/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 
 def home(request):
 
-    return render(request, 'wordle/home.html')
+    context = {
+        'page': 'wordle-game'
+    }
+
+    return render(request, 'wordle/home.html', context)
 
 
 def about(request):
 
-    return render(request, 'wordle/about.html')
+    context = {
+        'page': 'wordle-about'
+    }
+
+    return render(request, 'wordle/about.html', context)
